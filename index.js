d601f97c2aa0cbdb88eb904c8a799eb5fd31bd06
sagar chawla <sagarchawla83@gmail.com>
2023-04-21 23:42:54 +0530

    configured more for mobile devices

--- index.js
+++ index.js
@@ -33,16 +33,18 @@
   }
 
   // Attach event listeners
-  var article = document.querySelector("article");
-  article.addEventListener("pointerup", function (event) {
-    var selectedText = getSelectedText();
-    if (selectedText.length > 0) {
-      var x = event.pageX;
-      var y = event.pageY;
-      showSelectedTextMenu(x, y);
-    //   document.querySelector(".selected-text-menu").style.display = "block";
-    }
-  });
+var article = document.querySelector("article");
+article.addEventListener("pointerup", handleUserInput);
+article.addEventListener("touchend", handleUserInput);
+
+function handleUserInput(event) {
+  var selectedText = getSelectedText();
+  if (selectedText.length > 0) {
+    var x = event.pageX || event.changedTouches[0].pageX;
+    var y = event.pageY || event.changedTouches[0].pageY;
+    showSelectedTextMenu(x, y);
+  }
+}
 
   var shareTwitterButton = document.querySelector(".share-twitter-button");
   shareTwitterButton.addEventListener("click", function (event) {
